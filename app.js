423f16d54e2cab62176f975fd8da0c5aac99d568
Aaron Liao <steven610381@gmail.com>
2020-10-18 19:17:58 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -3,17 +3,26 @@ const app = express()
 const exhbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const db = mongoose.connection
-
+const exhpbs=require('express-handlebars')
+const bodyParser=require('body-parser')
 
 mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.get('/', (req, res) => {
-  res.send('aaaaaa')
+  res.render('index')
 })
 
 app.listen(3000, () => {
   console.log('APP is running on http://localhost:3000')
 })
+
+app.engine('hbs',exhbs({defaultLayout:'main',extname:'hbs'}))
+
+app.set('view engine','hbs')
+
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended:true}))
 
 db.on('error', () => {
   console.log('db error')
--- models/list.js
+++ models/list.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema=mongoose.Schema
+//define data structure
 const listSchema=new Schema(
   {id:Number,require:true},
   {name:String,require:true},
